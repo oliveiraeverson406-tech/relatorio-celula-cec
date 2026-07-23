0b97a5286ab6558feb8fe3c72fffdbcf4502a15a
oliveiraeverson406-tech <oliveiraeverson406@gmail.com>
2026-07-23 17:05:44 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'celula-cache-v1';
+const ARQUIVOS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ARQUIVOS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((chaves) =>
+      Promise.all(chaves.filter((c) => c !== CACHE_NAME).map((c) => caches.delete(c)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resposta) => resposta || fetch(event.request))
+  );
+});
