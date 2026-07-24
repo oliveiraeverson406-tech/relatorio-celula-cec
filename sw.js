87e7a42093f45b7dcd267a33a06f7f7d2f4bad1d
oliveiraeverson406-tech <oliveiraeverson406@gmail.com>
2026-07-24 15:55:56 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'celula-cache-v1';
+const CACHE_NAME = 'celula-cache-v2';
 const ARQUIVOS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
